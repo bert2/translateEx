80701fcc043a168a2b6893a89b50c7f7eba6f55b
Robert Hofmann <roberthofmann2@outlook.de>
2016-06-15 21:28:44 +0200

    Remove dead code

--- ExceptionMessageTranslator/LongestCommonSubsequence.fs
+++ ExceptionMessageTranslator/LongestCommonSubsequence.fs
@@ -11,12 +11,3 @@ let get list1 list2 =
 
 let strGet string1 string2 =
      System.String.Concat<char> (get (string1 |> List.ofSeq) (string2 |> List.ofSeq))
-
-let getLength string1 string2 =
-    let rec getLength' xs ys =
-        match (xs, ys) with
-        | ([], ys) -> 0
-        | (xs, []) -> 0
-        | (x::xs, y::ys) when x = y -> 1 + getLength' xs ys
-        | (x::xs, y::ys) -> max (getLength' (x::xs) ys) (getLength' xs (y::ys))
-    getLength' (string1 |> List.ofSeq |> List.rev) (string2 |> List.ofSeq |> List.rev)
