986e3b172497d55c543e6c1a8ad674c93054f686
Robert Hofmann <roberthofmann2@outlook.de>
2016-07-17 15:58:24 +0200

    Upgrade to .NET framework 4.6.1

--- Tests/LongestCommonSubsequenceTests.fs
+++ Tests/LongestCommonSubsequenceTests.fs
@@ -15,19 +15,19 @@ let ``.. of "ABCDE" and "ABXDY" is "ABD"``() =
     Assert.Equal("ABD", lcs "ABCDE" "ABXDY")
 
 [<Property(QuietOnSuccess = true, EndSize = maxLength, MaxTest = numTests)>]
-let ``.. has identity property`` (x:char list) = 
-    lcs' x x = x
+let ``.. has identity property`` (NonNull x) = 
+    lcs x x = x
 
 [<Property(QuietOnSuccess = true, EndSize = maxLength, MaxTest = numTests)>]
-let ``.. has absorber property`` (x:char list) = 
-    lcs' x [] = [] && lcs' [] x = []
+let ``.. has absorber property`` (x:NonNull<string>) = 
+    lcs x.Get "" = "" && lcs "" x.Get = ""
 
 [<Property(QuietOnSuccess = true, EndSize = maxLength, MaxTest = numTests)>]
-let ``.. has idempotence property`` (x:char list) (y:char list) = 
-    let lcs = lcs' x y
-    lcs' x lcs = lcs && lcs' y lcs = lcs
+let ``.. has idempotence property`` (x:NonNull<string>) (y:NonNull<string>) = 
+    let result = lcs x.Get y.Get
+    lcs x.Get result = result && lcs y.Get result = result
 
 [<Property(QuietOnSuccess = true, EndSize = maxLength, MaxTest = numTests)>]
-let ``.. starts with the char that has been prepended to both intputs`` (x:char list) (y:char list) c = 
-    let lcs = lcs' (c::x) (c::y)
-    List.head lcs = c    
+let ``.. starts with the char that has been prepended to both intputs`` (NonNull x) (NonNull y) c = 
+    let result = lcs (c.ToString() + x) (c.ToString() + y)
+    (Seq.head result) = c    
