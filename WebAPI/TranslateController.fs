2ee4d4e59703af60ed20ebed32b4a251a18a1dc5
Robert Hofmann <roberthofmann2@outlook.de>
2016-07-25 14:22:47 +0200

    Create working web service interface; Fix assembly version hell

--- WebAPI/TranslateController.fs
+++ WebAPI/TranslateController.fs
@@ -3,11 +3,15 @@
 open System.Net
 open System.Net.Http
 open System.Web.Http
+open Language
+open Translator
 
 /// Retrieves values.
 type TranslateController() =
     inherit ApiController()
 
     /// Gets all values.
-    member this.Get() = 
-        this.Request.CreateResponse(HttpStatusCode.OK, "test")
+    [<Route("translate/{targetLang?}/{sourceLang?}/{message?}")>]
+    member this.Get targetLang sourceLang message = 
+        let translatedMessage = translate (Language.create targetLang) (Language.create sourceLang) message
+        this.Request.CreateResponse(HttpStatusCode.OK, translatedMessage)
