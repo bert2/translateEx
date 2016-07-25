11e34ffbe399ac9f8c5fd5e1757b365d336a725d
Robert Hofmann <roberthofmann2@outlook.de>
2016-07-25 12:59:14 +0200

    Add empty Web API project

---
+++ WebAPI/TranslateController.fs
@@ -0,0 +1,13 @@
+﻿module TranslateController
+
+open System.Net
+open System.Net.Http
+open System.Web.Http
+
+/// Retrieves values.
+type TranslateController() =
+    inherit ApiController()
+
+    /// Gets all values.
+    member this.Get() = 
+        this.Request.CreateResponse(HttpStatusCode.OK, "test")
