3ad20e30e552a89e0adf66b3ec8ab1eb1907171d
Robert Hofmann <roberthofmann2@outlook.de>
2016-07-25 14:29:08 +0200

    Remove superflous "translate" from route

--- WebAPI/TranslateController.fs
+++ WebAPI/TranslateController.fs
@@ -6,12 +6,10 @@ open System.Web.Http
 open Language
 open Translator
 
-/// Retrieves values.
 type TranslateController() =
     inherit ApiController()
 
-    /// Gets all values.
-    [<Route("translate/{targetLang?}/{sourceLang?}/{message?}")>]
+    [<Route("{targetLang?}/{sourceLang?}/{message?}")>]
     member this.Get targetLang sourceLang message = 
         let translatedMessage = translate (Language.create targetLang) (Language.create sourceLang) message
         this.Request.CreateResponse(HttpStatusCode.OK, translatedMessage)
